9501bd58ee29017e6a5247396aafee39c22510a2
5onchangwoo <scw9410@gmail.com>
2023-02-15 18:26:16 +0900

    mbti 검사 페이지 12차 수정
    3. 문제 설문 유형 랜덤 변경

--- src/components/buttons/QuestionButton2.js
+++ src/components/buttons/QuestionButton2.js
@@ -17,37 +17,40 @@ export default ({seq, step, choice}) => {
                       justifyItems="center"
                       justifyContent="space-around"
                       alignContent="center"
+                      alignItems="center"
                 >
                     <Grid item xs={2}>
-                        <RadioButtonSize1 label={["매우", <br key={seq}/>, "그렇다"]} seq={seq} group={step}
+                        <RadioButtonSize1 label={["정말", <br key={seq}/>, "그렇다"]} seq={seq} group={step}
                                           value="3"
                                           checked={choice === "3"}
+                                          size={100}
                         />
                     </Grid>
-                    <Divider orientation="vertical"/>
                     <Grid item xs={2}>
                         <RadioButtonSize1 label="그렇다" seq={seq} group={step} value="1"
                                           checked={choice === "1"}
+                                          size={70}
                         />
                     </Grid>
-                    <Divider orientation="vertical"/>
                     <Grid item xs={2}>
-                        <RadioButtonSize1 label="보통" seq={seq} group={step} value="0"
+                        <RadioButtonSize1 label="반반이다" seq={seq} group={step} value="0"
                                           checked={choice === "0"}
+                                          size={50}
                         />
                     </Grid>
-                    <Divider orientation="vertical"/>
                     <Grid item xs={2}>
                         <RadioButtonSize1 label="아니다" seq={seq} group={step} value="-1"
                                           checked={choice === "-1"}
+                                          size={70}
                         />
 
                     </Grid>
-                    <Divider orientation="vertical"/>
                     <Grid item xs={2}>
-                        <RadioButtonSize1 label={["매우", <br key={seq}/>, "아니다"]} seq={seq} group={step}
+                        <RadioButtonSize1 label={["정말아니다", <br key={seq}/>, "아니다"]} seq={seq} group={step}
                                           checked={choice === "-3"}
-                                          value="-3"/>
+                                          value="-3"
+                                          size={100}
+                        />
                     </Grid>
                 </Grid>
             </RadioGroup>
--- src/components/buttons/RadioButtonSize1.js
+++ src/components/buttons/RadioButtonSize1.js
@@ -5,8 +5,10 @@ import {SET_MBTI_CHOICE} from "../../modules/MbtiReducer";
 import {NEXT_STEPPER} from "../../modules/StepperReducer";
 import {useState} from "react";
 import {debounce} from "lodash";
+import {styled} from "@mui/material/styles";
 
-export default function RadioButtonSize1({label, group, value, key, seq, checked}) {
+
+export default function RadioButtonSize1({label, group, value, key, seq, checked, size}) {
     const dispatch = useDispatch();
     const handleOnClick = debounce((e) => {
         dispatch({type: SET_MBTI_CHOICE, payload: {seq: seq, index: group, choice: e.target.value}});
@@ -15,7 +17,13 @@ export default function RadioButtonSize1({label, group, value, key, seq, checked
 
     return (
         <>
-            <FormControlLabel control={<Radio/>}
+            <FormControlLabel control={<Radio color="primary"
+                                              sx={{
+                                                  '& .MuiSvgIcon-root': {
+                                                      fontSize: size,
+                                                  },
+                                              }}
+                                       />}
                               value={value}
                               label={label}
                               key={key}
