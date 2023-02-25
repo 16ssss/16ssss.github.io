551a82ad9b820ccbe655a449a9552cb29a3d1488
5onchangwoo <scw9410@gmail.com>
2023-02-25 22:05:27 +0900

    MUI TEST reboot 1차 (3)
    1. footer 추가

---
+++ src/components/features/footers/QuestionFooter.js
@@ -0,0 +1,13 @@
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import {useMediaQuery, useTheme} from "@mui/material";
+
+export default () => {
+    return (
+        <Box marginTop={5}>
+            <Typography variant="caption">
+                Copyright 2023. sixteens All pictures cannot be copied without permission.
+            </Typography>
+        </Box>
+    );
+}
--- src/components/layouts/QuestionLayout.js
+++ src/components/layouts/QuestionLayout.js
@@ -1,12 +1,14 @@
 import {Outlet} from "react-router-dom";
 import {Container} from "@mui/material";
 import QuestionHeader from "../features/headers/QuestionHeader";
+import QuestionFooter from "../features/footers/QuestionFooter";
 
 export default () => {
     return (
         <Container maxWidth="sm" >
             <QuestionHeader/>
             <Outlet/>
+            <QuestionFooter/>
         </Container>
     )
 }
